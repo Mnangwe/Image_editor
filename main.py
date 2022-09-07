c27e5a3064d1be00065877515ffc9ab0e0aaab3b
Azabenathi Pupuma <pupuma.azabenathi@gmail.com>
2022-09-07 09:12:18 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from importlib.resources import path
+from PIL import Image, ImageEnhance, ImageFilter
+import os
+
+path = './imgs'
+pathOut  = '/editedImgs'
+
+for filename in os.listdir(path):
+    img = Image.open(f'{path}/{filename}')
+    
+    edit = img.filter(ImageFilter.SHARPEN).convert("L")
+    factor = 0.9
+    enhancer = ImageEnhance.Contrast(edit)
+    edit = enhancer.enhance(factor)
+    clean_name = os.path.splitext(filename)[0]
+    edit.save(f'.{pathOut}/{clean_name}_edited.jpg')
